5c2df5e9006449e0c4e308fc1cc30f04eea98253
Munchy6152 <onyinyemonchie@gmail.com>
2024-02-12 14:25:11 +0100

    new_commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,20 @@ module.exports = {
     
   ],
   theme: {
+    fontWeight :{
+      medium: '500',
+    },
     extend: {
       colors:{
         "bookmark-purple": "#5267DF",
         "bookmark-red": "#FA5959",
-        "bookmark-blue": "#242AA5",
+        "bookmark-blue": "rgba(36, 42, 69, 1);",
         "bookmark-grey": "#9194A2",
         "bookmark-white": "#f7f7f7",
       },
     },
     fontFamily:{
-      Poppins:["Poppins, san-serif"],
+      Rubik:["Rubik, sans-serif;"],
     },
     container:{
       center: true,
